18fa41c9a37b9619eaf828dec2ccf40e4b126768
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2025-06-29 19:41:19 -0300

    chore: define tempo de expiração do JWT para 1 hora

--- codigo-fonte/pd-backend/server.js
+++ codigo-fonte/pd-backend/server.js
@@ -13,7 +13,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // manter para testes locais
-      "https://pd-nilfisk.onrender.com", // novo: liberar produção
       "https://pdnilfisk.com", // novo: liberar produção
     ],
     credentials: true,
